f4f246e4fc934293a709b23aa8efdb0eeffaad3a
milkbacon <milk_candela@hotmail.com>
2021-07-25 23:59:20 -0500

    Subplot on predict.py

--- predict.py
+++ predict.py
@@ -1,12 +1,16 @@
-import pickle
-
-nombre = 'modeloKmedias.pkl'
-with open(nombre, 'rb') as file:
-    modelo = pickle.load(file)
+from pickle import load
+modelo = load(open('modeloKmedias.pkl', 'rb'))
 
 import os
 import pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
+from pickle import load
+from sklearn.decomposition import PCA
+import math
+
+pd.options.mode.chained_assignment = None
 
 def getDF(tipo, carpeta):
     camino = 'datos/' + tipo + '/' + carpeta + '/'
@@ -58,100 +62,129 @@ def getDFTomas(tipo, carpetas):
 dfPPG, l_PPG = getDFTomas('Empatica',['Resultados Primera Toma', 'Resultados Segunda Toma', 'Resultados Cuarta Toma'])
 dfEEG, l_EEG = getDFTomas('EEG',['Toma 1', 'Toma 2', 'Toma 4'])
 dfCV, l_CV = getDFTomas('Emociones',['Resultados Primera Toma DLIB', 'Resultados Segunda Toma DLIB', 'Resultados Cuarta Toma DLIB'])
-
-def filtDF(df, nombre, num, toma):
-    valores = [nombre, num, toma]
-    col_name = ['Nombre', 'Num', 'Toma']
-    for i, val in enumerate(valores):
-        if val is None:
-            continue
-        else:
-            df = df[df[col_name[i]] == val]
-    df.index = range(df.shape[0])
-    return(df)
-nombre, num, toma = (None, None, 2)
-dfCV, dfEEG, dfPPG = map(filtDF, [dfCV, dfEEG, dfPPG], [nombre] * 3, [num] * 3, [toma] * 3)
-
-print(dfEEG.shape, dfPPG.shape, dfCV.shape)
-
-np.random.seed(0)
-goodIDX = np.random.choice(dfEEG.index, size = dfPPG.shape[0], replace = False)
-print(dfEEG)
-print(goodIDX)
-dfEEG = dfEEG.iloc[goodIDX, :]
-
-file1 = open('columnasEEG.txt', 'r')
-columnasEEG = str(file1.readlines()).strip("[]'").split(',')
-file1.close()
-
-file1 = open('columnasPPG.txt', 'r')
-columnasPPG = str(file1.readlines()).strip("[]'").split(',')
-file1.close()
-
-print(columnasPPG)
-print(columnasEEG)
-
-dfEEG = dfEEG[columnasEEG]
-dfPPG = dfPPG[columnasPPG]
-
-print(dfEEG.shape, dfPPG.shape, dfCV.shape)
-print(dfEEG.head())
-print(dfPPG.head())
-print(dfCV.head())
-
-from pickle import load
-
-def sigmoid(x):
-    return(1/(1+np.exp(x)))
-def prom(x):
-    x = x[x != 'Pass']
-    if len(x) == 0:
-        return({'cNeu' : 0, 'cSur' : 0, 'cSad' : 0, 'cHap' : 0, 'cFea' : 0, 'cAng' : 0})
-    prop = {'cNeu' : len(x[x == 'neutral'])/len(x), # 0 Neutral
-              'cSur' : len(x[x == 'surprise'])/len(x), # 1 Surprised
-              'cSad' : len(x[x == 'sad'])/len(x), # 2 Sad
-              'cHap' : len(x[x == 'happy'])/len(x), # 3 Happy
-              'cFea' : len(x[x == 'fear'])/len(x), # 4 Fear
-              'cAng' : len(x[x == 'angry'])/len(x) # 6 Angry
-    }
-    return(prop)
-def CombDFs(dfPPG, dfCV, dfEEG):
-    from sklearn.preprocessing import StandardScaler
-    import math
-    #dfPPG.sdsd = pd.to_numeric(dfPPG.sdsd)
-    #dfPPG = dfPPG.dropna(axis = 1, how = 'any').drop('Segment_Indices', axis = 1).reset_index()
-
-    del dfPPG['Segment_Indices']
-
-    def df_to_prom(df, ID):
-        df.drop(['Nombre', 'Num', 'Toma', 'Ses'], axis=1, inplace=True)
-        columnas = df.columns
-        escalador = load(open('esc' + ID + '.pkl', 'rb'))
-        df = pd.DataFrame(escalador.transform(df))
-        df.columns = columnas
-        df = df.applymap(sigmoid)
+dfs = [dfCV, dfEEG, dfPPG]
+def plot_filt(dfs, nombre, num, toma, a):
+    def filtDF(df, nombre, num, toma):
+        valores = [nombre, num, toma]
+        col_name = ['Nombre', 'Num', 'Toma']
+        for i, val in enumerate(valores):
+            if val is None:
+                continue
+            else:
+                df = df[df[col_name[i]] == val]
+        df.index = range(df.shape[0])
         return(df)
-    dfPPG = df_to_prom(dfPPG, 'PPG')
-    dfEEG = df_to_prom(dfEEG, 'EEG')
-    print(dfEEG)
-    dfCV['Minute'] = (dfCV.Second/60).apply(math.ceil)
-    dfCV['Bin'] = pd.cut(x = dfCV.Minute, bins = len(dfPPG.index), labels = [x for x in range(1, len(dfPPG.index) + 1)])
+    dfCV, dfEEG, dfPPG = map(filtDF, dfs, [nombre] * 3, [num] * 3, [toma] * 3)
 
-    proporciones = pd.DataFrame(dfCV.groupby('Bin').EmotionDetected.apply(func = prom)).unstack().reset_index()
-    del proporciones['Bin']
-    proporciones = proporciones['EmotionDetected']
+    np.random.seed(0)
+    goodIDX = np.random.choice(dfEEG.index, size = dfPPG.shape[0], replace = False)
+    dfEEG = dfEEG.iloc[goodIDX, :]
 
-    dfcomb = pd.concat([dfPPG, proporciones, dfEEG], axis = 1)
-    return(dfcomb)
-combdf = CombDFs(dfPPG, dfCV, dfEEG)
-print(combdf)
+    file1 = open('columnasEEG.txt', 'r')
+    columnasEEG = str(file1.readlines()).strip("[]'").split(',')
+    file1.close()
 
-from sklearn.decomposition import PCA
-df = PCA(2).fit_transform(combdf)
+    file1 = open('columnasPPG.txt', 'r')
+    columnasPPG = str(file1.readlines()).strip("[]'").split(',')
+    file1.close()
 
-pred = modelo.predict(df)
-print(pred)
-print({'Probability_0' : str(round((pred == 0).sum()/len(pred) * 100, 2)) + ' %',
-       'Probability_1': str(round((pred == 1).sum()/len(pred) * 100, 2)) + ' %',
-       'Probability_2': str(round((pred == 2).sum()/len(pred) * 100, 2)) + ' %',
-       'Mode': max(set(list(pred)), key = list(pred).count)})
+    dfEEG = dfEEG[columnasEEG]
+    dfPPG = dfPPG[columnasPPG]
+
+    def sigmoid(x):
+        return(1/(1+np.exp(x)))
+    def prom(x):
+        x = x[x != 'Pass']
+        if len(x) == 0:
+            return({'cNeu' : 0, 'cSur' : 0, 'cSad' : 0, 'cHap' : 0, 'cFea' : 0, 'cAng' : 0})
+        prop = {'cNeu' : len(x[x == 'neutral'])/len(x), # 0 Neutral
+                  'cSur' : len(x[x == 'surprise'])/len(x), # 1 Surprised
+                  'cSad' : len(x[x == 'sad'])/len(x), # 2 Sad
+                  'cHap' : len(x[x == 'happy'])/len(x), # 3 Happy
+                  'cFea' : len(x[x == 'fear'])/len(x), # 4 Fear
+                  'cAng' : len(x[x == 'angry'])/len(x) # 6 Angry
+        }
+        return(prop)
+    def CombDFs(dfPPG, dfCV, dfEEG):
+        del dfPPG['Segment_Indices']
+        def df_to_prom(df, ID):
+            df.drop(['Nombre', 'Num', 'Toma', 'Ses'], axis=1, inplace=True)
+            columnas = df.columns
+            escalador = load(open('esc' + ID + '.pkl', 'rb'))
+            df = pd.DataFrame(escalador.transform(df))
+            df.columns = columnas
+            df = df.applymap(sigmoid)
+            return(df)
+        dfPPG = df_to_prom(dfPPG, 'PPG')
+        dfEEG = df_to_prom(dfEEG, 'EEG')
+
+        dfCV['Minute'] = (dfCV.loc[:,'Second']/60).apply(math.ceil)
+        dfCV['Bin'] = pd.cut(x = dfCV.loc[:,'Minute'], bins = len(dfPPG.index), labels = [x for x in range(1, len(dfPPG.index) + 1)])
+
+        proporciones = pd.DataFrame(dfCV.groupby('Bin').EmotionDetected.apply(func = prom)).unstack().reset_index()
+        del proporciones['Bin']
+        proporciones = proporciones['EmotionDetected']
+
+        dfcomb = pd.concat([dfPPG, proporciones, dfEEG], axis = 1)
+        return(dfcomb)
+    combdf = CombDFs(dfPPG, dfCV, dfEEG)
+
+    df = PCA(2).fit_transform(combdf)
+
+    pred = modelo.predict(df)
+    dict_probas = {'Probability_0' : str(round((pred == 0).sum()/len(pred) * 100, 2)) + ' %',
+                   'Probability_1': str(round((pred == 1).sum()/len(pred) * 100, 2)) + ' %',
+                   'Probability_2': str(round((pred == 2).sum()/len(pred) * 100, 2)) + ' %',
+                   'Mode': max(set(list(pred)), key = list(pred).count)}
+    print(dict_probas)
+    # Class 0 : Blue
+    # Class 1 : Orange
+    # Class 2 : Green
+
+    file1 = open('dimensiones.txt', 'r')
+    rango_x, rango_y = str(file1.readlines()).strip("[]'").split('+++')
+    file1.close()
+
+    labels = modelo.predict(df)
+    centroids = modelo.cluster_centers_
+    u_labels = np.unique(labels)
+
+    colores = ['#1f77b4', '#ff7f0e', '#2ca02c']
+    for i in u_labels:
+        a.scatter(df[labels == i , 0] , df[labels == i , 1] , label = 'Class ' + str(i) + ' (' + dict_probas['Probability_' + str(i)] + ')', color = colores[i])
+    atributos = [nombre, num, toma, str(dict_probas['Mode'])]
+    for i, atributo in enumerate(atributos):
+        if atributo is None:
+            atributos[i] = 'All'
+        atributos[i] = str(atributos[i])
+    a.set_title('Predicted Class on: ' + 'Name: ' + atributos[0] + ', ' + 'Num: ' + atributos[1] + ', ' + 'Take: ' + atributos[2] + ', ' + 'Mode: ' + atributos[3])
+    a.set_xlabel('PCA 1')
+    a.set_ylabel('PCA 2')
+    #ax = plt.gca()
+    a.set_xlim([float(rango_x.split(',')[0]), float(rango_x.split(',')[1])])
+    a.set_ylim([float(rango_y.split(',')[0]), float(rango_y.split(',')[1])])
+    a.legend()
+    #plt.show()
+
+def plot_names():
+    fig = plt.figure()
+    gs = fig.add_gridspec(3,2, wspace = 0.1, hspace=0.3)
+    axs = gs.subplots(sharex=True, sharey=True)
+    plot_filt(dfs, 'DJ', None, None, axs[0,0])
+    plot_filt(dfs, 'DT', None, None, axs[1,0])
+    plot_filt(dfs, 'ES', None, None, axs[2,0])
+    plot_filt(dfs, 'MJ', None, None, axs[0,1])
+    plot_filt(dfs, 'MT', None, None, axs[1,1])
+    plot_filt(dfs, 'ST', None, None, axs[2,1])
+    plt.show()
+
+def plot_takes():
+    fig = plt.figure()
+    gs = fig.add_gridspec(2, 2, wspace = 0.1, hspace=0.3)
+    axs = gs.subplots(sharex=True, sharey=True)
+    plot_filt(dfs, None, None, 1, axs[0,0])
+    plot_filt(dfs, None, None, 2, axs[0,1])
+    #plot_filt(dfs, None, None, 3, axs[0,1])
+    plot_filt(dfs, None, None, 4, axs[1,1])
+    plt.show()
+plot_takes()
